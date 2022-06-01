164da7b48e2ae0647d37b1aa1b643fc201648428
Alexandr <alexandr.lo@outlook.com>
2022-06-01 12:31:55 +0300

    Add hotkeys

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 
 import FullScreenPlayButton from "components/FullScreenPlayButton";
+import HotKeysManager from "components/HotKeysManager";
 import Notifications from "components/Notifications";
 import Sounds from "components/Sounds";
 import Timer from "components/Timer";
@@ -11,6 +12,7 @@ function App() {
       <Notifications />
       <Sounds />
       <FullScreenPlayButton />
+      <HotKeysManager />
       <Timer />
     </>
   );
